b52245bffdc5eae52217da584f1924062f305e34
CalvinGe <83850429+CalvinGe@users.noreply.github.com>
2021-05-25 22:39:57 +0800

    Update main.c
    SG的main.c

--- main.c
+++ main.c
@@ -3,14 +3,16 @@
 char * ifs=" \t\n";
 
 int main() {
-struct Data data1;
+
 char path[] = "D:\\C\\C_program\\shortpath\\miniversion.txt";
 FILE * fp;
 fp = fopen(path,"r");
-char line[100];
-while(fgets(line,sizeof(line),fp)!=NULL){
-    data1 = get_data(line, ifs);
-    printf("%s %s %d\n", data1.name1, data1.name2, data1.score);
-}
+struct Net net;
+net = create_net(fp);
+int i,j;
+for(i=0;i<net.number;i++)
+    for(j=0;j<net.number;j++)
+        if(net.PPI[i][j]!=-1)
+        printf("%s %s %d\n",net.protein[i],net.protein[j],net.PPI[i][j]);
 return 0;
 }
